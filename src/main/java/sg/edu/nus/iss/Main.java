94ed1ca1843b8729f6b9195332aa0611a240c16d
antsimch <anthonysimch@gmail.com>
2023-05-08 13:24:53 +0800

    list, add, delete, exit

--- src/main/java/sg/edu/nus/iss/Main.java
+++ src/main/java/sg/edu/nus/iss/Main.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,69 @@ public class Main
 {
     public static void main( String[] args )
     {
+        Scanner scan = new Scanner(System.in);
+        String option = "";
+        String input = "";
+        List<String> cart = new ArrayList<>();
+
+        System.out.println("Welcome to your shopping cart");
         
+        while (!"exit".equals(option)) {
+            System.out.print(">");
+            option = scan.next().toLowerCase();
+            input = scan.nextLine();
+    
+            switch (option) {
+                case "list" -> list(cart);
+                case "add" -> add(cart, input);
+                case "delete" -> delete(cart, input);
+                case "exit" -> System.out.println("See you again!");
+                default -> System.out.println("Available options are list, add, delete & exit");
+            }
+        }
+
+        scan.close();
+    }
+
+    public static void list(List<String> cart) {
+        if (!cart.isEmpty()) {
+            int index = 0;
+            for (String item: cart) {
+                index++;  
+                System.out.println(index + ". " + item);
+            }
+        } else {
+            System.out.println("Your cart is empty");
+        }
+    }
+
+    public static void add(List<String> cart, String input) {
+        if (!input.isEmpty()) {
+            String[] items = input.split(",");
+            for (String item : items) {
+                if (!cart.contains(item.trim().toLowerCase())) {
+                    cart.add(item.trim().toLowerCase());
+                    System.out.println(item.trim().toLowerCase() + " added to cart");
+                } else {
+                    System.out.println("You have " + item.trim().toLowerCase() + " in your cart");
+                }
+            }
+        } else {
+            System.out.println("No items entered");
+        }
+    }
+
+    public static void delete(List<String> cart, String input) {
+        if (!input.isEmpty()) {
+            int index = Integer.parseInt(input.trim());
+            if ((index > 0) && (index < cart.size())) {
+                System.out.println(cart.get(index - 1) + " removed from cart");
+                cart.remove(index - 1);
+            } else {
+                System.out.println("Incorrect item index");
+            }
+        } else {
+            System.out.println("No index entered");
+        }
     }
 }
